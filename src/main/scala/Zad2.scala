207beb6f0246faffb200d46b8a5de9598e537fe5
Svyatoslav Shypka <svyat200909@gmail.com>
2024-01-19 11:08:41 +0100

    Initial commit

---
+++ src/main/scala/Zad2.scala
@@ -0,0 +1,35 @@
+object Zad2 {
+
+  trait Debug {
+    def debugName(): Unit = {
+      println(s"Klasa: ${getClass.getSimpleName}")
+    }
+  }
+
+  class Point(varx: Int, vary: Int) extends Debug {
+    var x: Int = varx
+    var y: Int = vary
+    var a: String = "jakiś tekst"
+  }
+
+  class Student(varindex: Int, varname: String) extends Debug {
+    var index: Int = varindex
+    var name: String = varname
+  }
+
+  class Przedmiot(varnumer: Int, varnazwa: String) extends Debug {
+    var numer: Int = varnumer
+    var nazwa: String = varnazwa
+  }
+
+  def main(args: Array[String]): Unit = {
+    var p: Point = new Point(3, 4)
+    p.debugName()
+
+    var student: Student = new Student(5, "Kamil")
+    student.debugName()
+
+    var przedmiot: Przedmiot = new Przedmiot(1, "Paradygmaty Programowania")
+    przedmiot.debugName()
+  }
+}
